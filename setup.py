0762758e1971ff08c6e2e30d359fec5231b34b61
Kamil Mankowski (kam193) <kam193@wp.pl>
2020-05-10 23:37:32 +0200

    Add building package

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+
+def readme():
+    with open("README.md", "r") as file:
+        return file.read()
+
+
+def requirements():
+    with open("requirements.in") as file:
+        return file.read().splitlines()
+
+
+setup(
+    name="todo-ms-client",
+    version="0.0-dev",
+    description="Unofficial Python library for MS ToDo API",
+    long_description=readme(),
+    install_requires=requirements(),
+    url="https://github.com/kam193/todo-ms-client",
+    author="Kamil Mańkowski",
+    author_email="kam193@wp.pl",
+    license="MIT",
+    packages=find_packages(),
+    zip_safe=False,
+)
