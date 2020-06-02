4d4cb39ec783f81b0a493c5cc66d20491187ab7e
Kamil Mankowski (kam193) <kam193@wp.pl>
2020-06-02 21:12:17 +0200

    Bump version to 0.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,8 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 
 ## [Unreleased]
 
+## [0.0.2] - 2020-05-02
+
 ### Added
 
 - `WebBrowserProvider` can display custom messages
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def requirements():
 
 setup(
     name="todo-ms-client",
-    version="0.0.1",
+    version="0.0.2",
     description="Unofficial Python library for MS ToDo API",
     long_description=readme(),
     long_description_content_type="text/markdown",
