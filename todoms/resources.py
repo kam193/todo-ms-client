3aa098992341a63ab7fe1d6ee7aeda6964ccc146
Kamil Mankowski (kam193) <kam193@wp.pl>
2020-06-28 16:06:07 +0200

    Extract class for dict convertable objects

--- todoms/resources.py
+++ todoms/resources.py
@@ -15,6 +15,8 @@ from .converters import (
 )
 from .filters import and_, ne
 
+from .convertable import BaseConvertableObject
+
 
 class ResourceAlreadyCreatedError(Exception):
     """This resource is already created. Prevent duplicate"""
@@ -28,7 +30,7 @@ class TaskListNotSpecifiedError(Exception):
     """TaskList id must be set before create task"""
 
 
-class Resource(ABC):
+class Resource(BaseConvertableObject, ABC):
     """Base Resource for any other"""
 
     ENDPOINT = ""
@@ -53,19 +55,6 @@ class Resource(ABC):
         """Delete object in API"""
         self._client.delete(self)
 
-    def to_dict(self):
-        """Convert resource into dict accepted by API"""
-        data_dict = {}
-
-        for attr in self.ATTRIBUTES:
-            if isinstance(attr, AttributeConverter):
-                value = getattr(self, attr.local_name, None)
-                data_dict[attr.original_name] = attr.back_converter(value)
-            else:
-                data_dict[attr] = getattr(self, attr, None)
-
-        return data_dict
-
     @property
     def managing_endpoint(self):
         return (furl(self.ENDPOINT) / self.id).url
@@ -75,28 +64,8 @@ class Resource(ABC):
         return getattr(self, "_id", None)
 
     @classmethod
-    def create_from_dict(cls, client, data_dict: dict):
-        init_arguments = {}
-        private_attributes = {}
-
-        def store_attribute(name, value):
-            if name.startswith("_"):
-                private_attributes[name] = value
-            else:
-                init_arguments[name] = value
-
-        for attr in cls.ATTRIBUTES:
-            if isinstance(attr, AttributeConverter):
-                value = attr.obj_converter(data_dict.get(attr.original_name))
-                store_attribute(attr.local_name, value)
-            else:
-                store_attribute(attr, data_dict.get(attr))
-
-        obj = cls(client, **init_arguments)
-        for attr, value in private_attributes.items():
-            setattr(obj, attr, value)
-
-        return obj
+    def create_from_dict(cls, client, data_dict):
+        return super().create_from_dict(data_dict, client=client)
 
     @classmethod
     def handle_list_filters(cls, *args, **kwargs):
